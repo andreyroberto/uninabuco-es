deee964743f4409e7f83de1d059a88fd4c797bc9
andreyroberto <48372956+andreyroberto@users.noreply.github.com>
2019-08-28 15:45:03 +0100

    Update elevador.java

--- elevador.java
+++ elevador.java
@@ -1,76 +1,101 @@
 public class Elevador{
   private int andar_atual;
-  private int total_andar;
-  private int capacidade;
-  private int quantidade;
-  private int inicializado;
+	private int total_andares;
+	private int quantidade;
+	private int capacidade;
+	private boolean inicializado;
 
+  //construtor
   public Elevador(){
-    this.inicializado = 0;
+    this.inicializado = false;
   }
 
-  public int getAndar_atual(){
-    return this.andar_atual;
-  }
-  public void setAndar_atual(int andar){
-    if(this.inicializado != 0){
-      this.andar_atual = andar;
-    }
-  }
-  public int getTotal_andar(){
-    return this.total_andar;
-  }
-  public void setTotal_andar(int total){
-    if(this.inicializado != 0){
-      this.total_andar = total;
-    }
-  }
-  public int getCapacidade(){
-    return this.capacidade;
-  }
-  public void setCapacidade(int capacidade){
-    if(this.inicializado != 0){
-      this.capacidade = capacidade;
-    }
-  }
-  public int getQuantidade(){
-    return this.quantidade;
-  }
-  public void setQuantidade(int quantidade){
-    if(this.inicializado != 0){
-      this.quantidade = quantidade;
-    }
-  }
+  //getters
+	public int getAndar_atual() {
+		return andar_atual;
+	}
+	public int getTotal_andares() {
+		return total_andares;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public int getCapacidade() {
+		return capacidade;
+	}
+	public boolean getInicializado() {
+		return inicializado;
+	}
 
-  public void inicializa(int capacidade, int total){
-    if(this.inicializado == 0){
-      this.capacidade = capacidade;
-      this.total_andar = total;
-      this.andar_atual = 0;
-      this.quantidade = 0;
-      this.inicializado = 1;
-    }
-  }
-
-  public void sobe(){
-    if(this.andar_atual < this.total_andar){
-      this.andar_atual += 1;
-    }
-  }
-  public void desce(){
-    if(this.andar_atual > 0){
-      this.andar_atual -= 1;
-    }
-  }
-  public void entra(){
-    if(this.getQuantidade() < this.getCapacidade()){
-      this.setQuantidade(this.getQuantidade()+1);
-    }
-  }
-  public void sai(){
-    if(this.quantidade > 0){
-      this.quantidade--;
-    }
-  }
-
+  //metodos
+	public void inicializa(int capacidade, int totalAndares){
+		if(this.inicializado == false){
+			this.capacidade = capacidade;
+			this.total_andares = totalAndares;
+			this.andar_atual = 0; this.quantidade = 0; this.inicializado = true;
+		}
+	}
+	
+	public void sobe(){
+		if(this.andar_atual < this.total_andares && this.inicializado == true){
+			this.andar_atual += 1;
+		}
+	}
+	public void desce(){
+		if(this.andar_atual >= 1 && this.inicializado == true){
+			this.andar_atual -= 1;
+		}
+	}
+	public void entra(){
+		if(this.quantidade < this.capacidade && this.inicializado == true){
+			this.quantidade += 1;
+		}
+	}
+	public void sai(){
+		if(this.getQuantidade() > 0 && this.inicializado == true){
+			this.quantidade -= 1;
+		}
+	}
+	
+	public void qualAndar(){
+		int a = this.andar_atual;
+		if(this.inicializado != false){
+			if (a == 0)
+				System.out.println("O elevador está no térreo.");
+			else
+				System.out.println("O elevador está no " + a + ". andar.");
+		}
+	}
+	
+	public void cabeMaisUm(){
+		if(this.inicializado == true){
+			if(this.quantidade == 0)
+				System.out.println("O elevador está vazio.");
+			else if(this.quantidade == 1)
+				System.out.print("Tem " + this.getQuantidade() + " pessoa no elevador. ");
+			else
+				System.out.print("Tem " + this.getQuantidade() + " pessoas no elevador. ");
+			
+			if(this.quantidade > 0 && this.quantidade < this.capacidade){
+				System.out.println( "Ainda podem entrar " + (this.capacidade - this.quantidade) + " pessoas.");
+			}
+		}
+	}
+	
+	public void chamarElevador(int andar){
+		if(andar != this.andar_atual && andar > 0 && andar <= this.total_andares){
+			if(andar < this.andar_atual){
+				System.out.println("Descendo...");
+				for(int i=this.andar_atual; i >= andar; i--){
+					System.out.println(i + "...");
+				}
+			}else{
+				System.out.println("Subindo...");
+				for(int i=this.andar_atual; i <= andar; i++){
+					System.out.println(i + "...");
+				}
+			}
+			this.andar_atual = andar;
+		}
+	}
 }
